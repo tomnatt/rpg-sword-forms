93db6f2726bba1035bd2299415ef5948dacccd29
tomnatt <github@tomnatt.com>
2022-02-20 17:26:40 +0000

    Add feature tests for edit and new pages

--- spec/features/sword_form_feature_spec.rb
+++ spec/features/sword_form_feature_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Accessing the sword forms pages', type: :feature do
-  before :each do
+  scenario 'seeing the list of games on the index page' do
     create(:sword_form, name: 'Form 1')
     create(:sword_form, name: 'Form 2')
     create(:sword_form, name: 'Form 3')
-  end
 
-  scenario 'seeing the list of games on the index page' do
     visit sword_forms_path
 
     expect(page).to have_content 'Sword forms'
@@ -17,5 +15,23 @@ RSpec.describe 'Accessing the sword forms pages', type: :feature do
     expect(page).to have_content 'Form 1'
     expect(page).to have_content 'Form 2'
     expect(page).to have_content 'Form 3'
+  end
+
+  scenario 'editing one of the sword forms' do
+    create(:sword_form, name: 'Form 1', description: 'Test sword form 1')
+
+    visit edit_sword_form_path(SwordForm.find_by(name: 'Form 1'))
+
+    expect(page).to have_field 'Name', type: 'text', with: 'Form 1'
+    expect(page).to have_field 'Description', type: 'textarea', with: 'Test sword form 1'
+    expect(page).to have_button 'Save my form'
+  end
+
+  scenario 'adding one of the new sword forms' do
+    visit new_sword_form_path
+
+    expect(page).to have_field 'Name', type: 'text'
+    expect(page).to have_field 'Description', type: 'textarea'
+    expect(page).to have_button 'Save my form'
   end
 end

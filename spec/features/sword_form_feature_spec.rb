eeecbac786f401ea4710d18d4f3a0d9528053661
tomnatt <github@tomnatt.com>
2022-03-18 16:08:37 +0000

    Add Tag output to home page, and supporting tests

--- spec/features/sword_form_feature_spec.rb
+++ spec/features/sword_form_feature_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 # rubocop:disable Metrics/BlockLength
 RSpec.describe 'Accessing the sword forms pages', type: :feature do
   scenario 'seeing the list of forms on the index page' do
-    create(:sword_form, name: 'Form 1')
-    create(:sword_form, name: 'Form 2')
-    create(:sword_form, name: 'Form 3')
+    form1 = create(:sword_form, name: 'Form 1')
+    form2 = create(:sword_form, name: 'Form 2')
+    form3 = create(:sword_form, name: 'Form 3')
+    tag1 = create(:tag, name: 'Stab')
+    tag2 = create(:tag, name: 'Thrust')
+    form1.tags << tag1
+    form3.tags << tag1
+    form3.tags << tag2
 
     visit sword_forms_path
 
@@ -13,9 +18,19 @@ RSpec.describe 'Accessing the sword forms pages', type: :feature do
     expect(page).to_not have_link('New sword form', href: new_sword_form_path)
 
     # Check the page lists all the forms
-    expect(page).to have_content 'Form 1'
-    expect(page).to have_content 'Form 2'
-    expect(page).to have_content 'Form 3'
+    expect(page).to have_content form1.name
+    expect(page).to have_content form2.name
+    expect(page).to have_content form3.name
+
+    # Check the tags are being displayed
+    expect(page).to have_selector(:xpath, form_tag_selector(form1.name, tag1.name))
+    expect(page).to have_selector(:xpath, form_tag_selector(form3.name, tag1.name))
+    expect(page).to have_selector(:xpath, form_tag_selector(form3.name, tag2.name))
+  end
+
+  def form_tag_selector(form, tag)
+    # Select the <td> with the form name, go two cells right and check text contains tag name
+    ".//td[text() [normalize-space() = '#{form}'] ]/following-sibling::td[2][contains(text(), '#{tag}')]"
   end
 
   scenario 'seeing the link to create a new form when authenicated on the index page' do

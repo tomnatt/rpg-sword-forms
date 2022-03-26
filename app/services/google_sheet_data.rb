71f3632a5cebdf4ea9b88059738e87ebee153bf2
tomnatt <github@tomnatt.com>
2022-03-26 16:23:54 +0000

    Create Google Sheet reading service object and supporting tests

--- app/services/google_sheet_data.rb
+++ app/services/google_sheet_data.rb
@@ -1,41 +1,60 @@
 class GoogleSheetData
+  attr_reader :cached_tags
+
   # On creation, read the Google Sheet
   def initialize(account_key = nil, spreadsheet = nil)
+    @cached_tags = []
     return if account_key.nil? || spreadsheet.nil?
 
     # Get the sheet - see README for expected format
     session = GoogleDrive::Session.from_service_account_key(account_key)
     @data_sheet = session.spreadsheet_by_key(spreadsheet).worksheets.first
 
-    read_to_csv
-  end
-
-  def read_to_csv
-    # Drop the first line as they are headings
-    @data_sheet.rows.drop(1).each do |row|
-      puts row[0]
-    end
+    cache_existing_tags
+    create_objects
   end
 
   def create_objects
+    # Drop the first line as they are headings
+    @data_sheet.rows.drop(1).each do |row|
+      tags = create_or_fetch_tags(row[2])
+      create_form(row[0], row[1], tags)
+    end
+  end
+
+  def create_or_fetch_tags(tag_string)
+    # Needs to return an array or Tag objects or nil
+    return nil if tag_string.blank?
+
+    # tag_string should be a comma separated string, so split and operate
+    form_tags = []
+    tag_string.split(',').each do |t|
+      t.chomp!
+      # If new, create and add to cache else collect from cache
+      form_tags << (tag_from_cache(t).presence || create_tag(t))
+    end
+    form_tags
   end
 
   def create_tag(tag_name)
     tag = Tag.new(name: tag_name)
     tag.save! if tag.valid?
+    @cached_tags << tag
+    tag
   end
 
-  # TODO: tag caching - remember tests!
-  def cache_tags
-    @tags = Tag.all
+  def cache_existing_tags
+    @cached_tags = Tag.all.to_a
   end
 
-  def tag(tag_name)
-    # filter @tags and return correct object
+  def tag_from_cache(tag_name)
+    # filter @cached_tags and return correct object - name is unique so should be able to take first
+    @cached_tags.select { |t| t.name == tag_name }.first
   end
 
-  def create_form(form_name, form_description)
+  def create_form(form_name, form_description, tags)
     form = SwordForm.new(name: form_name, description: form_description)
     form.save! if form.valid?
+    form.tags << tags unless tags.nil?
   end
 end
--- spec/services/google_sheet_data_spec.rb
+++ spec/services/google_sheet_data_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe GoogleSheetData do
 
   it 'successfully creates a Tag' do
     expect(Tag.all.count).to eq 0
-    @gsd.create_tag('Tag1')
+    tag = @gsd.create_tag('Tag1')
+    expect(tag.name).to eq 'Tag1'
     expect(Tag.all.count).to eq 1
 
     tag = Tag.all.first
@@ -26,23 +27,130 @@ RSpec.describe GoogleSheetData do
     expect(SwordForm.all.count).to eq 0
     form_name = 'Form1'
     form_desc = 'Description'
-    @gsd.create_form(form_name, form_desc)
+    @gsd.create_form(form_name, form_desc, nil)
     expect(SwordForm.all.count).to eq 1
 
     form = SwordForm.all.first
     expect(form.name).to eq form_name
     expect(form.description).to eq form_desc
+    expect(form.tags.count).to eq 0
+  end
+
+  it 'successfully creates a SwordForm with Tags' do
+    tag1 = create(:tag, name: 'Tag1')
+    tag2 = create(:tag, name: 'Tag2')
+    tags = [tag1, tag2]
+
+    form_name = 'Form1'
+    form_desc = 'Description'
+    @gsd.create_form(form_name, form_desc, tags)
+    expect(SwordForm.all.count).to eq 1
+
+    form = SwordForm.all.first
+    expect(form.name).to eq form_name
+    expect(form.description).to eq form_desc
+    expect(form.tags.count).to eq 2
+
+    # Tags are not guaranteed to be sorted, so will be one of them
+    expect(form.tags[0].name).to match(/Tag[1,2]/)
+    expect(form.tags[1].name).to match(/Tag[1,2]/)
   end
 
   it 'gracefully fails to create a SwordForm' do
-    @gsd.create_form('Form1', 'Description')
+    @gsd.create_form('Form1', 'Description', nil)
     expect(SwordForm.all.count).to eq 1
-    @gsd.create_form('Form1', 'Description')
+    @gsd.create_form('Form1', 'Description', nil)
     expect(SwordForm.all.count).to eq 1
   end
 
-  it 'successfully creates a Tagged Form'
-  it 'successfully caches the Tags'
-  it 'can find a Tag in the cache'
+  it 'converts a string to Tag objects' do
+    # Nil or empty string should return nil
+    tag_string = nil
+    expect(@gsd.create_or_fetch_tags(tag_string)).to be nil
+
+    tag_string = ''
+    expect(@gsd.create_or_fetch_tags(tag_string)).to be nil
+
+    # At this point there should be no Tags in the database
+    expect(Tag.all.count).to eq 0
+
+    # Single entry should return single-item array of Tag objects saved in database
+    tag_string = 'Defensive'
+    @gsd.create_or_fetch_tags(tag_string)
+    expect(@gsd.cached_tags.count).to eq 1
+    expect(@gsd.cached_tags[0].name).to match(/Defensive/)
+    expect(Tag.all.count).to eq 1
+
+    # Double entry should return single-item array of Tag objects saved in database
+    tag_string = 'Defensive, Form'
+    @gsd.create_or_fetch_tags(tag_string)
+    expect(@gsd.cached_tags.count).to eq 2
+    expect(@gsd.cached_tags[0].name).to match(/Defensive|Form/)
+    expect(@gsd.cached_tags[0].name).to match(/Defensive|Form/)
+    expect(Tag.all.count).to eq 2
+  end
+
+  it 'correctly fetches Tag from cache when present' do
+    # Create a new Tag and prove it is in the cache and the database
+    tag = create(:tag, name: 'Tag1')
+    @gsd.cache_existing_tags
+    expect(Tag.all.count).to eq 1
+    expect(@gsd.cached_tags.count).to eq 1
+
+    # Remove the Tag from the database and prove it's still in the cache
+    tag.destroy!
+    expect(Tag.all.count).to eq 0
+    expect(@gsd.cached_tags.count).to eq 1
+
+    # Fetch the tag and prove we get it, and it still doesn't exist in the database
+    tags = @gsd.create_or_fetch_tags('Tag1')
+    expect(Tag.all.count).to eq 0
+    expect(@gsd.cached_tags.count).to eq 1
+    expect(tags.count).to eq 1
+    expect(tags.first.name).to eq 'Tag1'
+  end
+
+  it 'correctly creates a new Tag and caches if not already in cache' do
+    create(:tag, name: 'Tag1')
+    @gsd.cache_existing_tags
+    expect(Tag.all.count).to eq 1
+    expect(@gsd.cached_tags.count).to eq 1
+
+    tags = @gsd.create_or_fetch_tags('Tag2')
+    expect(Tag.all.count).to eq 2
+    expect(@gsd.cached_tags.count).to eq 2
+    expect(tags.count).to eq 1
+    expect(tags.first.name).to eq 'Tag2'
+
+    tags = @gsd.create_or_fetch_tags('Tag1')
+    expect(Tag.all.count).to eq 2
+    expect(@gsd.cached_tags.count).to eq 2
+    expect(tags.count).to eq 1
+    expect(tags.first.name).to eq 'Tag1'
+  end
+
+  it 'successfully caches the Tags' do
+    create(:tag, name: 'Tag1')
+    create(:tag, name: 'Tag2')
+    create(:tag, name: 'Tag3')
+
+    @gsd.cache_existing_tags
+    expect(@gsd.cached_tags.count).to eq 3
+  end
+
+  it 'can find a Tag in the cache' do
+    create(:tag, name: 'Tag1')
+    @gsd.cache_existing_tags
+    expect(@gsd.cached_tags.count).to eq 1
+
+    tag = @gsd.tag_from_cache('Tag1')
+    expect(tag.present?).to be true
+    expect(tag.name).to eq 'Tag1'
+  end
+
+  it 'returns nil if no Tag in cache' do
+    tag = @gsd.tag_from_cache('NoTag')
+    expect(tag).to be nil
+  end
 end
 # rubocop:enable Metrics/BlockLength

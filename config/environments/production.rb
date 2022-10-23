67c1ec4fdee6e4f5af9ce7f8bf89fd765ac82eff
tomnatt <github@tomnatt.com>
2022-10-23 19:10:24 +0100

    Update email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.default_url_options = { host: ENV['APPLICATION_URL'] }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.sendgrid.net',
-  #   port:                 587,
-  #   user_name:            ENV['SENDGRID_USERNAME'],
-  #   password:             ENV['SENDGRID_PASSWORD'],
-  #   domain:               'heroku.com',
-  #   authentication:       :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.default_url_options = { host: ENV.fetch(['APPLICATION_URL'], nil) }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    user_name:            ENV.fetch(['SENDGRID_USERNAME'], nil),
+    password:             ENV.fetch(['SENDGRID_PASSWORD'], nil),
+    domain:               'koyeb.app',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

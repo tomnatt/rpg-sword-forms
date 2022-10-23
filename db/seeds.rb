d1e1ad4097847c8943e2dc386f8906f1a225b7a4
tomnatt <github@tomnatt.com>
2022-10-23 19:27:24 +0100

    Fix linter violations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
 # Create an admin user if it doesn't conflict with existing data
-admin_email = ENV['SF_EMAIL'] || 'admin@example.com'
-admin_password = ENV['SF_PASSWORD'] || 'password'
-admin_name = ENV['SF_NAME'] || 'admin'
+admin_email = ENV.fetch(['SF_EMAIL'], 'admin@example.com')
+admin_password = ENV.fetch(['SF_PASSWORD'], 'password')
+admin_name = ENV.fetch(['SF_NAME'], 'admin')
 u = User.new(email:    admin_email,
              password: admin_password,
              name:     admin_name)
 u.save! if u.valid?
 
 # Read in data from a Google spreadsheet (if account details and spreadsheet set) and save any that do not conflict
-return unless ENV['SWORD_FORM_SERVICE_ACCOUNT'] || ENV['GOOGLE_APPLICATION_CREDENTIALS']
-return unless ENV['SWORD_FORM_SPREADSHEET']
+sword_form_service_account = ENV.fetch(['SWORD_FORM_SERVICE_ACCOUNT'], nil)
+google_application_credentials = ENV.fetch(['GOOGLE_APPLICATION_CREDENTIALS'], nil)
+sword_form_spreadsheet = ENV.fetch(['SWORD_FORM_SPREADSHEET'], nil)
 
-if ENV['SWORD_FORM_SERVICE_ACCOUNT']
-  GoogleSheetData.new(ENV['SWORD_FORM_SERVICE_ACCOUNT'], ENV['SWORD_FORM_SPREADSHEET'])
+return unless sword_form_service_account || google_application_credentials
+return unless sword_form_spreadsheet
+
+if sword_form_service_account
+  GoogleSheetData.new(sword_form_service_account, sword_form_spreadsheet)
 else
-  GoogleSheetData.new(ENV['GOOGLE_APPLICATION_CREDENTIALS'], ENV['SWORD_FORM_SPREADSHEET'])
+  GoogleSheetData.new(google_application_credentials, sword_form_spreadsheet)
 end
